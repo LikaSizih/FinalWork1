545ec15b58358834cc9319c914f9e8487513a8dd
Lika Sizih <likasizih@mail.ru>
2022-11-14 22:51:25 +0300

    Добавлен код решения задачи

---
+++ task/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] arrayGiven = new string[4] { "1234", "1567", "-2", "computer science" };
+string[] arrayNew = new string[arrayGiven.Length];
+void ArrayTransfer(string[] arrayGiven, string[] arrayNew)
+{
+    int n = 3;
+    int j = 0;
+    for (int i = 0; i < arrayGiven.Length; i++)
+    {
+        if (arrayGiven[i].Length <= n)
+        {
+            arrayNew[j] = arrayGiven[i];
+            j++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+ArrayTransfer(arrayGiven, arrayNew);
+PrintArray(arrayNew);
